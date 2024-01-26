c358af17efbcb6dcab3ef9f899a73eae7dc06963
lihmmh <155299302+lihmmh@users.noreply.github.com>
2024-01-26 21:13:41 +0900

    Update bst_lihmmh.c

--- bst_lihmmh.c
+++ bst_lihmmh.c
@@ -1,8 +1,9 @@
-//일단 create랑 insert_node까지만 만들었음
-//나중에 다른 것도 할게요.. 힘들어요...
+//일단 create랑 insert_node, print_bst까지만 만들었음
+//다른건 나중에 추가할게요...
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct Node {
     int key;
@@ -10,7 +11,7 @@ typedef struct Node {
     struct Node* right;
 } node;
 
-typedef struct BST {
+typedef struct BST { //이거 구조체 안만들었어도 됐을거같은데.. 너무 나중에 알았음
     node* parent;
 }bst;
 
@@ -19,8 +20,10 @@ bst* create_bst() {
     if (new_bst == NULL) {
         printf("NOT ENOUGH MEMORY");
     }
-    new_bst->parent = NULL;
-    return new_bst;
+    else {
+        new_bst->parent = NULL;
+        return new_bst;
+    }
 }
 
 node* new_node(const int data)
@@ -68,4 +71,62 @@ void insert_node(bst* new_bst, const int a)
         }
     }
 }
+void print_bst(node* parent) {
+    if (parent == NULL) {
+        printf("NO DATA");
+    }
+    else {
+        
+        printf("%d", parent->key);
+        if (parent->left != NULL || parent->right != NULL)
+        {
+            printf("(");
+            if (parent->left != NULL) {
+                print_bst(parent->left);
+            }
+            if (parent->right != NULL) {
+                printf(",");
+                print_bst(parent->right);
+            }
+            printf(")");
+            
+        }
+        
+    }
+}
+   
+int main() { // 메인함수 그냥 내가 대충 만듦
+    bst* the_bst = create_bst();
+    int pick;
+    while (1) {
 
+        printf("select the menu:\n");
+        printf("1.insert data in the bst\n");
+        printf("2.print the bst\n");
+        scanf_s("%d", &pick);
+
+        switch (pick)
+        {
+        case 1:
+            printf("input the data:");
+            int data;
+            scanf_s("%d", &data);
+            insert_node(the_bst, data);
+            printf("(");
+            print_bst(the_bst->parent);
+            printf(")");
+            break;
+
+        case 2:
+            printf("------------\n");
+            printf("the bst:");
+            printf("(");
+            print_bst(the_bst->parent);
+            printf(")");
+            break;
+
+        default:
+            break;
+        }
+    }
+}
